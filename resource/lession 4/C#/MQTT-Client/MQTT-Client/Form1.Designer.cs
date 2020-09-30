83686cdd608e37ab54ded79d091bfe2e6c00ea14
Ngoc Phu Truong <ngocphu811@live.com>
2020-09-30 08:18:44 +0700

    add c# example

---
+++ resource/lession 4/C#/MQTT-Client/MQTT-Client/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace MQTT_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.Controller_ID_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.HostCbb = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Topic_pub_cmb = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.PubMessageTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.tmrUpdateUI = new System.Windows.Forms.Timer(this.components);
+            this.txtShow2 = new System.Windows.Forms.TextBox();
+            this.btn_publish = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(71, 697);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 19);
+            this.label4.TabIndex = 10;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(0, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(100, 23);
+            this.label12.TabIndex = 41;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ConnectButton.Location = new System.Drawing.Point(302, 12);
+            this.ConnectButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(179, 32);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // Controller_ID_txt
+            // 
+            this.Controller_ID_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Controller_ID_txt.Location = new System.Drawing.Point(245, 16);
+            this.Controller_ID_txt.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Controller_ID_txt.MaxLength = 128;
+            this.Controller_ID_txt.Name = "Controller_ID_txt";
+            this.Controller_ID_txt.Size = new System.Drawing.Size(46, 19);
+            this.Controller_ID_txt.TabIndex = 6;
+            this.Controller_ID_txt.Text = "1234";
+            this.Controller_ID_txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.HostCbb);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Controller_ID_txt);
+            this.groupBox1.Controls.Add(this.ConnectButton);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(4, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(855, 47);
+            this.groupBox1.TabIndex = 31;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connect";
+            // 
+            // HostCbb
+            // 
+            this.HostCbb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.HostCbb.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HostCbb.FormattingEnabled = true;
+            this.HostCbb.Items.AddRange(new object[] {
+            "localhost"});
+            this.HostCbb.Location = new System.Drawing.Point(42, 16);
+            this.HostCbb.Name = "HostCbb";
+            this.HostCbb.Size = new System.Drawing.Size(171, 21);
+            this.HostCbb.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(219, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // Topic_pub_cmb
+            // 
+            this.Topic_pub_cmb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Topic_pub_cmb.FormattingEnabled = true;
+            this.Topic_pub_cmb.Items.AddRange(new object[] {
+            "monitor",
+            "control"});
+            this.Topic_pub_cmb.Location = new System.Drawing.Point(64, 16);
+            this.Topic_pub_cmb.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Topic_pub_cmb.MaxDropDownItems = 3;
+            this.Topic_pub_cmb.Name = "Topic_pub_cmb";
+            this.Topic_pub_cmb.Size = new System.Drawing.Size(698, 24);
+            this.Topic_pub_cmb.TabIndex = 32;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_publish);
+            this.groupBox2.Controls.Add(this.PubMessageTextBox);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.Topic_pub_cmb);
+            this.groupBox2.Location = new System.Drawing.Point(4, 53);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(855, 93);
+            this.groupBox2.TabIndex = 33;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Publish";
+            // 
+            // PubMessageTextBox
+            // 
+            this.PubMessageTextBox.Location = new System.Drawing.Point(64, 44);
+            this.PubMessageTextBox.Name = "PubMessageTextBox";
+            this.PubMessageTextBox.Size = new System.Drawing.Size(698, 23);
+            this.PubMessageTextBox.TabIndex = 68;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Topic";
+            // 
+            // txtShow
+            // 
+            this.txtShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtShow.Location = new System.Drawing.Point(445, 167);
+            this.txtShow.Multiline = true;
+            this.txtShow.Name = "txtShow";
+            this.txtShow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtShow.Size = new System.Drawing.Size(408, 222);
+            this.txtShow.TabIndex = 40;
+            // 
+            // txtShow2
+            // 
+            this.txtShow2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtShow2.Location = new System.Drawing.Point(10, 167);
+            this.txtShow2.Multiline = true;
+            this.txtShow2.Name = "txtShow2";
+            this.txtShow2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtShow2.Size = new System.Drawing.Size(413, 222);
+            this.txtShow2.TabIndex = 42;
+            // 
+            // btn_publish
+            // 
+            this.btn_publish.Location = new System.Drawing.Point(774, 16);
+            this.btn_publish.Name = "btn_publish";
+            this.btn_publish.Size = new System.Drawing.Size(75, 51);
+            this.btn_publish.TabIndex = 69;
+            this.btn_publish.Text = "Publish";
+            this.btn_publish.UseVisualStyleBackColor = true;
+            this.btn_publish.Click += new System.EventHandler(this.btn_publish_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 421);
+            this.Controls.Add(this.txtShow2);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "HT-MQTT-CLENT";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_Closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox Controller_ID_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox Topic_pub_cmb;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox PubMessageTextBox;
+        private System.Windows.Forms.TextBox txtShow;
+        private System.Windows.Forms.ComboBox HostCbb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer tmrUpdateUI;
+        private System.Windows.Forms.TextBox txtShow2;
+        private System.Windows.Forms.Button btn_publish;
+    }
+}
+
---
+++ resource/lession 4/C#/MQTT-Client/MQTT-Client/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.IO.Ports;
+
+
+namespace MQTT_Client
+{
+    public partial class Form1 : Form
+    {
+        private delegate void myUICallBack(string myStr, TextBox ctl);
+        private delegate void myUI_clearCallBack(TextBox ctl);
+
+
+        static String[] subTopic = { "monitor" };
+        static byte[] qosLevel = {0};
+
+        //Variables for MQTT connection
+        static MqttClient client;
+
+        string str_broker_add = "";
+        string str_acc = "";
+        string str_pass = "";
+
+        string monitor_topic = "";
+        static int port = 1883;
+        string str_controller_ID = "";
+
+        string str_monitor_topic = "";
+
+        private void myUI(string myStr, TextBox ctl)
+        {
+            if (this.InvokeRequired)
+            {
+                myUICallBack myUpdate = new myUICallBack(myUI);
+                this.Invoke(myUpdate, myStr, ctl);
+            }
+            else
+            {
+                ctl.AppendText(myStr + Environment.NewLine);
+            }
+        }
+        private void myUI_clear(TextBox ctl)
+        {
+            if (this.InvokeRequired)
+            {
+                myUI_clearCallBack myUpdate = new myUI_clearCallBack(myUI_clear);
+                this.Invoke(myUpdate, ctl);
+            }
+            else
+            {
+                ctl.Clear();
+            }
+        }
+        private void myUI_text(string myStr, TextBox ctl)
+        {
+            if (this.InvokeRequired)
+            {
+                myUICallBack myUpdate = new myUICallBack(myUI);
+                this.Invoke(myUpdate, myStr, ctl);
+            }
+            else
+            {
+                ctl.Text = myStr;
+            }
+        }
+
+        private void AppendTextBox(string txt)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox), new object[] { txt });
+                return;
+            }
+            txtShow.Text += txt;
+            txtShow.SelectionStart = txtShow.TextLength;
+            txtShow.ScrollToCaret();
+        }
+
+        /* Start Display Mqtt Message */
+        private delegate void displayMqttMsgCallback(string displayString, TextBox displayTextbox);
+
+        private void displayMqttMsg(string displayString, TextBox displayTextbox)
+        {
+            if (this.InvokeRequired)
+            {
+                displayMqttMsgCallback displayMqttMsgUpdate = new displayMqttMsgCallback(displayMqttMsg);
+                this.Invoke(displayMqttMsgUpdate, displayString, displayTextbox);
+            }
+            else
+            {
+                displayTextbox.AppendText(displayString + Environment.NewLine);
+            }
+        }
+
+        private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            AppendTextBox(Encoding.Default.GetString(e.Message));
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = this.ConnectButton;
+            Topic_pub_cmb.SelectedIndex = 1;
+        }
+
+        private void Form1_Closing(object sender, FormClosingEventArgs e)
+        {
+            if (client != null && client.IsConnected) client.Disconnect();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (ConnectButton.Text == "Connect")
+            {
+                try
+                {
+                    str_broker_add = HostCbb.Text;
+                    client = new MqttClient(str_broker_add, port, false, null);
+                    client.Connect(Guid.NewGuid().ToString(), str_acc, str_pass);
+                    client.MqttMsgPublishReceived += new MqttClient.MqttMsgPublishEventHandler(client_MqttMsgPublishReceived);
+
+
+                    client.Subscribe(subTopic, qosLevel);
+                }
+                catch
+                {
+                    label4.Text = "Can't connect to server";
+                }
+                if (client != null && client.IsConnected)
+                {
+
+                    label4.Text = "";
+                    tmrUpdateUI.Enabled = true;
+                    HostCbb.Enabled = false;
+                    Controller_ID_txt.Enabled = false;
+
+                    //switch connect button to disconnect button
+                    ConnectButton.Text = "Disconnect";
+
+                    //subscribe monitor topic
+                    str_controller_ID = Controller_ID_txt.Text;
+                    str_monitor_topic = monitor_topic + str_controller_ID;
+                }
+            }
+            else if (ConnectButton.Text == "Disconnect")
+            {
+                if (client != null && client.IsConnected) client.Disconnect();
+                HostCbb.Enabled = true;
+                Controller_ID_txt.Enabled = true;
+
+                //switch disconnect button to connect button
+                ConnectButton.Text = "Connect";
+            }
+        }
+
+        private void btn_publish_Click(object sender, EventArgs e)
+        {
+            byte[] pubData = new byte[5];
+            for (int i = 0;i < 5; i++){
+                pubData[i] = (byte)'A';
+            }
+
+            client.Publish("command", pubData);
+        }
+    }
+}
