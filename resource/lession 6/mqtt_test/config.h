6c3db179a94b81ef7a68faf3b6770723292040cb
Ngoc Phu Truong <ngocphu811@live.com>
2020-10-26 19:25:46 +0700

    update code

---
+++ resource/lession 6/mqtt_test/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#define   	CONFIG_LOGGER_EN         (1)
+
+#define		SENML_DOC_BUFFER_SIZE  120
+
+#define		MQTT_MONITOR_TOPIC  "arduino/monitor"
+#define 	MQTT_CONTROL_TOPIC  "arduino/control"
+
+#endif /* CONFIG_H_ */
